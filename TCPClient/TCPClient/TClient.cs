3b03101049cb12751fa63e9f41d85f4f25f48b16
Stephen Nguyen <stephen.nguyensn@gmail.com>
2017-07-23 14:54:58 -0700

    TCP Example

---
+++ TCPClient/TCPClient/TClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPClient
+{
+    class TClient
+    {
+        TcpClient client;
+        IPEndPoint ep;
+        NetworkStream stream;
+        public TClient()
+        {
+            ep = new IPEndPoint(IPAddress.Loopback, 12345);
+        }
+
+        private bool Connect()
+        {
+
+            try
+            {
+                client = new TcpClient();
+                client.Connect(ep);
+                stream = client.GetStream();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+        private void Close()
+        {
+            client.Close();
+        }
+
+        public bool SendMessage(string msg)
+        {
+            if (!Connect())
+            {
+                return false;
+            }
+            stream.Write(Encoding.ASCII.GetBytes(msg), 0, msg.Length);
+            stream.Close();
+            Close();
+            return true;
+        }
+    }
+}
---
+++ TCPServer/TCPServer/TServer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPServer
+{
+    class TServer
+    {
+        TcpListener serverSocket;
+
+        public TServer()
+        {
+            serverSocket = new TcpListener(IPAddress.Loopback, 12345);
+            serverSocket.Start();
+        }
+
+        public async Task<string> waitConnection()
+        {
+            TcpClient client = await serverSocket.AcceptTcpClientAsync();
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[256];
+            int len = await stream.ReadAsync(buffer, 0, 256);
+            //stream.Read(buffer, 0, 256);
+            string data = System.Text.Encoding.ASCII.GetString(buffer,0, len);
+
+            client.Close();
+
+            return data;
+        }
+    }
+}
